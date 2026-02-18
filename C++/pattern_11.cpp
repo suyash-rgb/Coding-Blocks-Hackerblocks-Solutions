10495650ad4b8e8bd69e1b7e3d7a87d1d471ceed
Suyash Baoney <suyashbaoney58@gmail.com>
2026-02-18 10:46:24 +0530

    row asymmetric pattern

---
+++ C++/pattern_11.cpp
@@ -0,0 +1,24 @@
+//Row Symmetric Pattern
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cout<<"Enter the number of rows: ";
+    cin>>n; //5
+
+    for(int i=1; i<=n; i++){ //rows
+        for(int j=1; j<=i; j++){
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+    for(int i=n-1; i>=1; i--){ 
+        for(int j=1; j<=i ; j++){
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+
+    return 0;
+}
