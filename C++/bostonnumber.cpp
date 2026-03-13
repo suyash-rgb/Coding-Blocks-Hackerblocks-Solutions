fee9e8119d498260f766d55d4adcfba08ed434f2
Suyash Baoney <suyashbaoney58@gmail.com>
2026-03-13 10:58:29 +0530

    Boston number

---
+++ C++/bostonnumber.cpp
@@ -0,0 +1,54 @@
+// Ex - 378
+// Prime Factoriation : 2*3*3*3*7 =18
+// Sum of digits : 3+7+8=18
+
+#include<iostream>
+using namespace std;
+
+//function to calculate sum of digits
+int sumOfDigits(int n){
+    int sum=0;
+    while(n!=0){
+        int k = n%10;
+        sum+=k;
+        n=n/10;
+    }
+    return sum;
+}
+bool isPrime(int n){
+    if(n<=1){
+        return false;
+    }
+    for(int i=2; i<=n/2; i++){ //mid-point theorem
+        if(n%i==0){
+            return false;
+        }
+    }
+    return true;
+}
+
+int main(){
+    int n;
+    cin>>n;
+    int lhs=sumOfDigits(n);
+    int rhs=0;
+    for(int i=2; i<n; i++){
+        if(isPrime(i)){
+            if(n%i==0){
+                while(n%i==0){
+                    rhs+=sumOfDigits(i);
+                    n=n/i;
+                }
+            }
+        }
+    }
+    if(n>1){ //if n is prime
+        rhs+=sumOfDigits(n);
+    }
+    if(lhs==rhs){
+        cout<<"Boston number"<<endl;
+    }
+    else{
+        cout<<"Not a Boston number"<<endl;
+    }
+}
