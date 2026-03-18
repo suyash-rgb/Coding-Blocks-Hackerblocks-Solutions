64cdd4050bb11883575b154145d4d018f69a3dbb
Suyash Baoney <suyashbaoney58@gmail.com>
2026-03-18 11:11:42 +0530

    vector pop

---
+++ C++/vector_pop.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+int main(){
+
+    vector<int> a ={6,7,8,9,10};
+    sort(a.begin(), a.end());
+    cout<<"sorting order"<<endl;
+    for(int i=0; i<a.size(); i++){
+        cout<<a[i]<<" ";
+    }   
+    cout<<endl;
+    cout<<"Reverse sorting order"<<endl;
+    sort(a.rbegin(), a.rend());
+    for(int i=0; i<a.size(); i++){
+        cout<<a[i]<<" ";
+    } 
+
+    //alternatively we can also use reverse() function to reverse the order of elements in the vector
+
+    cout<<endl<<"Reverse sorting order"<<endl;
+    vector<int> b = {1,2,3,4,5};
+    reverse(b.begin(), b.end());
+    for(int i=0; i<b.size(); i++){
+        cout<<b[i]<<" ";
+    }
+   
+
+    
+    return 0;
+}
