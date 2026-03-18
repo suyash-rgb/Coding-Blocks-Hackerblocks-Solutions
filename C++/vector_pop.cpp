29768f93673bbdd60fe5d02f78f3f448fd0735e7
Suyash Baoney <suyashbaoney58@gmail.com>
2026-03-18 11:17:18 +0530

    vector descending order

--- C++/vector_pop.cpp
+++ C++/vector_pop.cpp
@@ -26,6 +26,14 @@ int main(){
     for(int i=0; i<b.size(); i++){
         cout<<b[i]<<" ";
     }
+
+    cout<<endl;
+    cout<<"Descending ordrer"<<endl;
+    sort(a.begin(), a.end(), greater<int>());
+    for(int i=0; i<a.size(); i++){
+        cout<<a[i]<<" ";
+    }
+
    
 
     
